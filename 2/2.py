58f8c34efc4c94ac3d86b2b36ebc159ae0ee73cf
vasyamarusyak <vasya_marusyak@i.ua>
2018-11-27 20:23:58 +0200

    Lab2

---
+++ 2/2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 19:07:27 2018
+
+@author: pro
+"""
+
+x=input()
+y=input()
+z=input()
+
+x=int(x)
+y=int(y)
+z=int(z)
+
+k=(x-z/8)/(18.2-3.8**y+19.3)
+
+print(k)
